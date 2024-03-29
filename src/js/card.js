44e55270dcadd576b61b19986f262bce94c0b030
LisaMandarin <chu23001@byui.edu>
2024-03-29 18:25:22 +0800

    lint and format

--- src/js/card.js
+++ src/js/card.js
@@ -1,38 +1,41 @@
 export default class ShowDetail {
-    constructor(dataSource) {
-        this.dataSource = dataSource;
+  constructor(dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  init() {
+    this.renderCard(this.dataSource);
+  }
+
+  getItems(results) {
+    if (!Array.isArray(results)) {
+      return "";
     }
+    return results.join(", ");
+  }
 
-    init() {
-        this.renderCard(this.dataSource);
-        }
+  getValue(objects) {
+    const values = objects.map((obj) => obj.name);
+    return values.join(", ");
+  }
 
-    getItems(results) {
-        if (!Array.isArray(results)) {
-            return '';
-        }
-        return results.join(", ");
-    }
+  getStreamingData(result) {
+    //Check if result.streamingInfo.us is an array
+    if (!Array.isArray(result.streamingInfo.us)) return [];
+    return result.streamingInfo.us.map((obj) => ({
+      service: obj.service,
+      streamingType: obj.streamingType,
+      link: obj.link,
+    }));
+  }
 
-    getValue(objects) {
-        const values = objects.map((obj) => obj.name)
-        return values.join(", ");
-    }
-
-    getStreamingData(result) {    
-        //Check if result.streamingInfo.us is an array
-        if (!Array.isArray(result.streamingInfo.us)) return [];
-        return result.streamingInfo.us.map(obj => ({
-            service: obj.service,
-            streamingType: obj.streamingType,
-            link: obj.link 
-        }));
-        }
-
-    streamingTemplate(streamingData) {
-        if (!streamingData || streamingData.length === 0) return "⚠️⚠️No streaming services provided!";
-        return streamingData.map(data => 
-            `
+  streamingTemplate(streamingData) {
+    if (!streamingData || streamingData.length === 0)
+      return "⚠️⚠️No streaming services provided!";
+    return streamingData
+      .map(
+        (data) =>
+          `
             <div class="streaming-card">
                 <a href="${data.link}" target="_blank class="streaming-link">
                     <div class="streaming-img-container">
@@ -41,18 +44,30 @@ export default class ShowDetail {
                     </div>
                 </a>
             </div>
-            `
-            ).join("");
-        }
+            `,
+      )
+      .join("");
+  }
 
-    cardTemplate(title, imdbId, overview, cast, directors, genres, type, year, firstAirYr, lastAirYr) {
-        let timeFrameContent = `<span class="card-year title-font">(N/A<)/span>`
-        if (type === "movie") {
-            timeFrameContent = `<span class="card-year title-font">(${year})</span>`;
-        } else if (type === "series") {
-            timeFrameContent = `<span class="card-year title-font">(${firstAirYr}-${lastAirYr})</span>`;
-        } 
-        return `
+  cardTemplate(
+    title,
+    imdbId,
+    overview,
+    cast,
+    directors,
+    genres,
+    type,
+    year,
+    firstAirYr,
+    lastAirYr,
+  ) {
+    let timeFrameContent = `<span class="card-year title-font">(N/A<)/span>`;
+    if (type === "movie") {
+      timeFrameContent = `<span class="card-year title-font">(${year})</span>`;
+    } else if (type === "series") {
+      timeFrameContent = `<span class="card-year title-font">(${firstAirYr}-${lastAirYr})</span>`;
+    }
+    return `
             <div class="card-box"><a href="https://www.imdb.com/title/${imdbId}" class="title-font" target="_blank">${title} &nbsp;</a>${timeFrameContent}</div>
             <div class="card-box">${overview}</div>
             <div class="card-box">
@@ -72,35 +87,46 @@ export default class ShowDetail {
                 <div class="card-box-right">${type}</div>  
             </div>
         `;
-        } 
+  }
 
-    renderCard(result) {
-        const type = result.type;
-        const title = result.title;
-        const imdbId = result.imdbId;
-        const overview = result.overview;
-        const cast = this.getItems(result.cast);
-        const year = result.year;
-        const firstAirYr = result.firstAirYear;
-        const lastAirYr = result.lastAirYear;
-        const genres = this.getValue(result.genres);
-        const directors = this.getItems(result.directors);
-        const streamingData = this.getStreamingData(result);
-        
-        const cardHTML = this.cardTemplate(title, imdbId, overview, cast, directors, genres, type, year, firstAirYr, lastAirYr);
-        const cardContainer = document.createElement("div");
-        cardContainer.classList.add("cardContainer");
-        cardContainer.innerHTML = cardHTML;
+  renderCard(result) {
+    const type = result.type;
+    const title = result.title;
+    const imdbId = result.imdbId;
+    const overview = result.overview;
+    const cast = this.getItems(result.cast);
+    const year = result.year;
+    const firstAirYr = result.firstAirYear;
+    const lastAirYr = result.lastAirYear;
+    const genres = this.getValue(result.genres);
+    const directors = this.getItems(result.directors);
+    const streamingData = this.getStreamingData(result);
 
-        const streamingHTML = this.streamingTemplate(streamingData);
-        const streamingContainer = document.createElement("div");
-        streamingContainer.classList.add("streaming-container");
-        streamingContainer.innerHTML += streamingHTML;
+    const cardHTML = this.cardTemplate(
+      title,
+      imdbId,
+      overview,
+      cast,
+      directors,
+      genres,
+      type,
+      year,
+      firstAirYr,
+      lastAirYr,
+    );
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("cardContainer");
+    cardContainer.innerHTML = cardHTML;
 
-        const main = document.querySelector("main")
-        const hr = document.createElement("hr");
-        main.appendChild(cardContainer);
-        main.appendChild(streamingContainer);
-        main.appendChild(hr);
-    }
+    const streamingHTML = this.streamingTemplate(streamingData);
+    const streamingContainer = document.createElement("div");
+    streamingContainer.classList.add("streaming-container");
+    streamingContainer.innerHTML += streamingHTML;
+
+    const main = document.querySelector("main");
+    const hr = document.createElement("hr");
+    main.appendChild(cardContainer);
+    main.appendChild(streamingContainer);
+    main.appendChild(hr);
+  }
 }
--- src/js/data.js
+++ src/js/data.js
@@ -2,87 +2,84 @@ import ShowDetail from "./card.js";
 
 // ----------api information----------
 
-const baseURL = 'https://streaming-availability.p.rapidapi.com/';
+const baseURL = "https://streaming-availability.p.rapidapi.com/";
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'd3aba69b6fmsh17c1f11a47cb011p19a02bjsn5d6558abaadb',
-		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d3aba69b6fmsh17c1f11a47cb011p19a02bjsn5d6558abaadb",
+    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+  },
 };
 // ----------end of api information----------
-
 
 // ----------execute search----------
 
 async function performSearch() {
-    // Clear existing results before fetching new ones    
-    clearResult(); 
-    
-    const title = document.querySelector("#search-bar").value;
-    const country = document.querySelector("#select-country").value;
-    const show_type = document.querySelector("#select-show_type").value;
-    const output_language = "en";
-    const fullURL = `${baseURL}search/title?title=${encodeURIComponent(title)}&country=${country}&show_type=${show_type}&output_language=${output_language}`
-        
-    try {
-        const response = await fetch(fullURL, options);
-        if (!response.ok) throw new Error("Network response was not ok.");
-        const results = await response.json();
-        
-        if (Array.isArray(results.result)) {
-            results.result.forEach(result => {
-                new ShowDetail(result).init();
-            });
-        } else {
-            console.error("Expected results.result to be an array.")
-        }
-    } catch (error) {
-        console.error("Error: ", error);
+  // Clear existing results before fetching new ones
+  clearResult();
+
+  const title = document.querySelector("#search-bar").value;
+  const country = document.querySelector("#select-country").value;
+  const show_type = document.querySelector("#select-show_type").value;
+  const output_language = "en";
+  const fullURL = `${baseURL}search/title?title=${encodeURIComponent(title)}&country=${country}&show_type=${show_type}&output_language=${output_language}`;
+
+  try {
+    const response = await fetch(fullURL, options);
+    if (!response.ok) throw new Error("Network response was not ok.");
+    const results = await response.json();
+
+    if (Array.isArray(results.result)) {
+      results.result.forEach((result) => {
+        new ShowDetail(result).init();
+      });
+    } else {
+      console.error("Expected results.result to be an array.");
     }
+  } catch (error) {
+    console.error("Error: ", error);
+  }
 }
 
-document.querySelector(".search-button").addEventListener('click', performSearch);
+document
+  .querySelector(".search-button")
+  .addEventListener("click", performSearch);
 
-document.querySelector("#search-bar").addEventListener('keypress', function(event) {
+document
+  .querySelector("#search-bar")
+  .addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
-        performSearch();
+      performSearch();
     }
-});
+  });
 
 // ----------end of execute search----------
-
 
 // ----------clear result----------
 
 function clearResult() {
-    const mainElem = document.querySelector("main");
-    mainElem.innerHTML = "";
+  const mainElem = document.querySelector("main");
+  mainElem.innerHTML = "";
 }
 
 // ----------end of clear result----------
 
-
 // ----------search button animation----------
 const searchButton = document.querySelector(".search-button");
 searchButton.addEventListener("click", () => {
-    // Remove the class if it's already applied to ensure the animation can restart
-    searchButton.classList.remove("rubberBand");
-    
-    // Force a reflow to ensure the class removal is processed
-    void searchButton.offsetWidth;
+  // Remove the class if it's already applied to ensure the animation can restart
+  searchButton.classList.remove("rubberBand");
 
-    //Re-apply the class to trigger the animation
-    searchButton.classList.add("rubberBand");
+  // Force a reflow to ensure the class removal is processed
+  void searchButton.offsetWidth;
 
-})
+  //Re-apply the class to trigger the animation
+  searchButton.classList.add("rubberBand");
+});
 
 searchButton.addEventListener("animationend", () => {
-    searchButton.classList.remove("rubberBand");
+  searchButton.classList.remove("rubberBand");
 });
 
 // ----------end of search button animation----------
-
-
-
