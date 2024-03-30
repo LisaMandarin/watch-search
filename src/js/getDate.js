1cece1d960fb59845f30af1dfd10a1ccbc4c4aa2
LisaMandarin <chu23001@byui.edu>
2024-03-31 03:23:01 +0800

    visit count

--- src/js/getDate.js
+++ src/js/getDate.js
@@ -1,3 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let visitCount = localStorage.getItem("visitCount")
+    ? parseInt(localStorage.getItem("visitCount"), 10)
+    : 0;
+  visitCount++;
+  localStorage.setItem("visitCount", visitCount);
+  document.getElementById("visit-number").textContent = visitCount;
+});
+
 const today = new Date();
 const currentYear = today.getFullYear();
 
